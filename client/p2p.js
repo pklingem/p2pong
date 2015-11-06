b5e13578a8d19e1e177e2316c3ba9ccef0751d34
Scott McCormack <scott.mccormack@articulate.com>
2015-11-06 10:54:26 -0500

    Connect socket on current location

--- client/p2p.js
+++ client/p2p.js
@@ -1,6 +1,5 @@
 var P2P = require('socket.io-p2p');
-var io = require('socket.io-client');
-var socket = io('http://localhost:3030');
+var socket = require('socket.io-client')();
 
 var p2p = new P2P(socket);
 
